4f05b32e6598a96a2aae0bfd2706552359ff479b
Konstantin <kirilenkokonstantin2003@gmail.com>
2021-10-07 00:55:53 +0400

    first commit

---
+++ task4.cpp
@@ -0,0 +1,54 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "CHISLITEL(1) ILI ZNAMENATEL(2)? ";
+    cin >> a;
+    cout << "VVEDITE NOMER DNYA: ";
+    cin >> b;
+    if (a == 1) {
+        switch (b) {
+        case 1:
+            cout << 2;
+            break;
+        case 2:
+            cout << 3;
+            break;
+        case 3:
+            cout << 4;
+            break;
+        case 4:
+            cout << 2;
+            break;
+        case 5:
+            cout << 4;
+            break;
+
+        }
+    }
+    else {
+            switch (b) {
+            case 1:
+                cout << 2;
+                break;
+            case 2:
+                cout << 3;
+                break;
+            case 3:
+                cout << 4;
+                break;
+            case 4:
+                cout << 3;
+                break;
+            case 5:
+                cout << 3;
+                break;
+
+            }
+
+        }
+}
+
